f0324a85b3ca0a50f2bc02b74799e7bf99e553f0
Andy Meixner <andy.meixner@shineunited.com>
2018-02-09 00:16:38 -0600

    add meta deta to post header

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -8,13 +8,23 @@ class Post extends Component {
     super(props);
 
     this.state = {
+      apiRoute: 'http://cms-exercise.test/wp-json/wp/v2',
       post: '',
+
+      // initially set json keys that cause conflicts with react reserved keywords to empty strings
       post: {
         title: {
           rendered: ''
         },
         content: {
           rendered: ''
+        },
+        _embedded: {
+          author: [
+            {
+              name: ''
+            }
+          ]
         }
       }
     };
@@ -23,7 +33,7 @@ class Post extends Component {
   componentDidMount(){
      var postsEndpoint = '/posts/',
          postId = this.props.match.params.postId,
-         postURL = 'http://cms-exercise.test/wp-json/wp/v2' + postsEndpoint + postId + '?_embed';
+         postURL = this.state.apiRoute + postsEndpoint + postId + '?_embed';
 
     fetch(postURL)
       .then(res => res.json())
@@ -33,8 +43,6 @@ class Post extends Component {
         })
       })
   }
-
-
 
   render(){
     return(
@@ -46,6 +54,10 @@ class Post extends Component {
               <div className="col-lg-8 col-md-10 mx-auto">
                 <div className="post-heading">
                   <h1><div dangerouslySetInnerHTML={ {__html: this.state.post.title.rendered} } /></h1>
+                    <span className="meta">Posted by
+                      <span className="post-author"> { this.state.post._embedded.author[0].name}</span> on
+                      <span className="post-date"> <Moment format="MMM Do, YYYY">{ this.state.post.date }</Moment></span>
+                    </span>
                 </div>
               </div>
             </div>
@@ -55,9 +67,7 @@ class Post extends Component {
           <div className="container">
             <div className="row">
               <div className="col-lg-8 col-md-10 mx-auto">
-
                 <div dangerouslySetInnerHTML={ {__html: this.state.post.content.rendered} } />
-
               </div>
             </div>
           </div>
